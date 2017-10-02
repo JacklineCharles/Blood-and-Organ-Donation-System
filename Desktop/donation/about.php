e072edfd1b8f402d04496789033d7445518b35c3
JacklineCharles <wanjirajackie8@gmail.com>
2017-10-02 17:13:17 +0300

    Blood and Organ Donation Project

---
+++ Desktop/donation/about.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign In | Blood and Organ Donation System</title>
+ 
+    
+   <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+     .navbar-default {
+  background-color: #dc0303;
+  border-color: #dc0000;
+}
+.navbar-default .navbar-brand {
+  color: #fafeff;
+}
+.navbar-default .navbar-brand:hover,
+.navbar-default .navbar-brand:focus {
+  color: #fefefe;
+}
+.navbar-default .navbar-text {
+  color: #fafeff;
+}
+.navbar-default .navbar-nav > li > a {
+  color: #fafeff;
+}
+.navbar-default .navbar-nav > li > a:hover,
+.navbar-default .navbar-nav > li > a:focus {
+  color: #fefefe;
+}
+.navbar-default .navbar-nav > .active > a,
+.navbar-default .navbar-nav > .active > a:hover,
+.navbar-default .navbar-nav > .active > a:focus {
+  color: #fefefe;
+  background-color: #dc0000;
+}
+.navbar-default .navbar-nav > .open > a,
+.navbar-default .navbar-nav > .open > a:hover,
+.navbar-default .navbar-nav > .open > a:focus {
+  color: #fefefe;
+  background-color: #dc0000;
+}
+.navbar-default .navbar-toggle {
+  border-color: #dc0000;
+}
+
+
+
+   </style>
+  
+  </head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+     <a class="navbar-brand">Tumaini Blood and Organ donation portal</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.html">Home</a></li>
+      <li><a href="donorlogin.php">Donor Portal</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Staff Portal
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="doclogin.php">Doctor Portal</a></li>
+          <li><a href="adminlogin.php">Admin</a></li>
+       
+        </ul>
+      </li>
+  </div>
+</nav>
+<h3>Blood Donation</h3>
+<p><strong>Why donate blood?</strong></p>
+
+<ol type="1">
+<li>Someone needs it and you’ve got it why not. All it takes is the ability and the will.</li>
+<li>Where will it come from when you need it? If we all take up blood donation then we will be confident about its availability in case we or those we care about need it.</li>
+<li>It helps us live a disciplined lifestyle when we look up to the next time to donate. The sure result will be behavior change and the zeal to live a healthy lifestyle, and live on a healthy diet to ensure full health after donation.</li>
+</ol>
+<p><strong>Who can donate blood?</strong></p>
+<p>16 years to 65 years, above 50 kgs, safe sexual lifestyle and a healthy person with no chronic condtions such as asthma, diabetes, allergy etc.</p>
+<p><strong>Blood is Life. </strong></p>
+<ul type="disc">
+<li>Blood donation is a safe and confidential process.</li>
+<li>The whole process takes approximately 30 minutes, this includes registration, health checks and the actual donation.</li>
+<li>It is a requirement to have a meal and drink plenty of water or fluids at least 3 hours before you attend the session.</li>
+<li>You will be requested to fill out a brief questionnaire.</li>
+<li>You will have a brief session with the blood donor nurse to take your vitals.</li>
+<li>The phlebotomy staff will usher you to the donation couch where you will donate blood. This should take between 5 and 15 minutes.</li>
+</ul>
+<p><strong>Benefits of donating blood</strong></p>
+ <ul class="check">
+    <li>Free health screening: Every time you give blood, a medical technician, doctor or nurse will check your blood pressure and give you a free health screening.</li>
+    <li>Reduced risk of heart disease: Some doctors and scientists believe that giving blood may prevent heart disease. Although no conclusive evidence is available, numerous physicians have noted that blood donors typically have fewer cases of heart disease than non-donors. </li>
+    <li>Blood Donation giving the wonderful feeling of saving someone's life.</li>
+    <li>One can diet or remain fit by donating blood regularly. One pint of blood (450 ml) when donated burns 650 calories in donor’s body.</li>
+    <li>After donating blood, the count of blood cells decreases in our body, which stimulates the bone marrow to produce new red blood cells in order to replenish the loss. So, it stimulates the production of new blood cells and refreshes the system.</li>
+    <li>Regular blood donation helps especially males in loosing iron on regular basis. It helps in reducing the chance of a heart attack to one third.</li>
+    
+    
+    
+    
+    
+</ul>
+<p style="text-align: left;"><strong>Registration</strong></p>
+<p>This will be done by a donor clerk. A questionnaire will be given requiring the donors bio data, medical and social history. </p>
+<p><strong>Weight Check.</strong></p>
+<ul type="disc">
+<li>The minimum weight requirement for a blood donor is 50 kgs.</li>
+</ul>
+<p><strong>Blood Pressure and Haemoglobin (Hb) Check.</strong></p>
+<p>The blood donor nurse will check;</p>
+<ul type="disc">
+<li>Your blood pressure.</li>
+<li>If your Hb is adequate; An appropriate Hb for a donor has to be more than 12.5 grams per deciliter.</li>
+</ul>
+<p>Individuals not meeting the minimum donation requirements are either permanently or temporarily deferred.</p>
+<p><strong>Blood donation</strong></p>
+<ul type="disc">
+<li>You will be ushered to your donation couch.</li>
+<li>All the equipment used is sterile.</li>
+<li>One pint of blood or <strong>450 mls</strong> is collected. </li>
+<li>After donation you are advised to rest for at least 5 minutes to allow your blood flow to stabilize.</li>
+</ul>
+<p><strong>Refreshments</strong></p>
+<ul type="disc">
+<li>Refreshments will be served to replace fluids and give an energy boost.</li>
+</ul>
+<p><strong>Confidentiality</strong> </p>
+<p>The whole donation process is treated with maximum confidentiality. </p>   <span class="hits"><i class="icon-signal"></i>
+ </span>
+
+  
+
+</div>
+
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+           
+
+</body>
+</html>
+   
+      
